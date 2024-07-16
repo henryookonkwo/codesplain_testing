ed4ef11e5476d18e1a7428ed2caf1403c6735e4c
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-15 23:55:29 -0400

    Looping over assertions with more flexible queries

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,16 +1,24 @@
-import { screen, render } from '@testing-library/react';
-import RepositoriesSummary from './RepositoriesSummary';
+import { screen, render } from "@testing-library/react";
+import { expect } from "playwright/test";
+import RepositoriesSummary from "./RepositoriesSummary";
 
-test('displays the primary language of the repository', () => {
+test("displays information about the repository", () => {
   const repository = {
-    language: 'Javascript',
+    language: "Javascript",
     stargazers_count: 5,
     forks: 30,
     open_issues: 1,
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText('Javascript');
+  // const language = screen.getByText('Javascript');
 
-  expect(language).toBeInTheDocument();
+  // expect(language).toBeInTheDocument();
+
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
+
+    expect(element).toBeInTheDocument();
+  }
 });
