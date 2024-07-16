c96702356f8d0419fa171e5aeffc6bb4ab8a5312
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-16 16:06:23 -0400

    Fixed link icon to the github homepage for each repository

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,12 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
-jest.mock("../tree/FileIcon", () => {
-  // Content of FileIcon.js
-  return () => {
-    return "File Icon Component";
-  };
-});
+// jest.mock("../tree/FileIcon", () => {
+//   // Content of FileIcon.js
+//   return () => {
+//     return "File Icon Component";
+//   };
+// });
 
 function renderComponent() {
   const repository = {
@@ -18,16 +18,21 @@ function renderComponent() {
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
+
   render(
     <MemoryRouter>
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+  return { repository }; //to allow us to call repository in the test function
 }
 
 test("shows a link to the github homepage for this repository", async () => {
-  renderComponent();
+  // screen.debug()
+  const { repository } = renderComponent();
 
-  screen.debug();
-  // await screen.findByRole('img', { name: 'Javascript' });
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = screen.getByRole("link", { name: /github repository/i });
+  expect(link).toHaveAttribute("href", repository.html_url);
 });
