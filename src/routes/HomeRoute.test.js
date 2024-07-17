fda362b320bb11b371e14efb6765926aab4c6676
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-17 11:33:37 -0400

    Inspecting the component state

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,19 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
-import { MemoryRouter } from 'react-router-dom';
-import HomeRoute from './HomeRoute';
+import { render, screen } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { MemoryRouter } from "react-router-dom";
+import HomeRoute from "./HomeRoute";
 
 const handlers = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const query = req.url.searchParams.get('q');
+  rest.get("/api/repositories", (req, res, ctx) => {
+    const query = req.url.searchParams.get("q");
     console.log(query);
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: 'full name!!!' },
-          { id: 2, full_name: 'other name!!!' },
+          { id: 1, full_name: "full name!!!" },
+          { id: 2, full_name: "other name!!!" },
         ],
       })
     );
@@ -29,4 +29,16 @@ afterEach(() => {
 });
 afterAll(() => {
   server.close();
+});
+
+test("renders two links for each language", async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  // Loop over each language
+  // For each language, make sure we see two links
+  // Assert that the links have the appropriate full_name
 });
