02a866c97f7a5e1fcc9dc02fb4685c8e24eb8cbb
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-18 09:53:55 -0400

    Making a Reusable Create Server Function

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,35 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
 import { MemoryRouter } from "react-router-dom";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-const handlers = [
-  rest.get("/api/repositories", (req, res, ctx) => {
-    const query = req.url.searchParams.get("q");
-    console.log(query);
-
-    return res(
-      ctx.json({
+createServer([
+  {
+    path: "/api/repositories",
+    res: (req) => {
+      const language = req.url.searchParams.get("q").split("language:")[1];
+      return {
         items: [
-          { id: 1, full_name: "full name!!!" },
-          { id: 2, full_name: "other name!!!" },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
-      })
-    );
-  }),
-];
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-afterEach(() => {
-  server.resetHandlers();
-});
-afterAll(() => {
-  server.close();
-});
+      };
+    },
+  },
+]);
 
 test("renders two links for each language", async () => {
   render(
@@ -39,6 +26,29 @@ test("renders two links for each language", async () => {
   );
 
   // Loop over each language
-  // For each language, make sure we see two links
-  // Assert that the links have the appropriate full_name
+  const languages = [
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "python",
+    "java",
+  ];
+  for (let language of languages) {
+    // For each language, make sure we see two links
+    const links = await screen.findAllByRole("link", {
+      name: new RegExp(`${language}_`),
+    });
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+    expect(links[0]).toHaveAttribute("href", `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute("href", `/repositories/${language}_two`);
+  }
 });
+
+const pause = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 100);
+  });
