83af8a7b6c53ce24c1566074fab09c479ef87c19
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-07-18 18:27:01 -0400

    Fixed SWR caching issue

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -1,16 +1,56 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { SWRConfig } from "swr";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
 async function renderComponent() {
   render(
-    <MemoryRouter>
-      <AuthButtons />
-    </MemoryRouter>
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <MemoryRouter>
+        <AuthButtons />
+      </MemoryRouter>
+    </SWRConfig>
   );
   await screen.findAllByRole("link");
 }
+
+describe("when user is signed in", () => {
+  // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 3, email: "asdf@asdf.com" } };
+      },
+    },
+  ]);
+
+  test("sign in and sign up are not visible", async () => {
+    await renderComponent();
+
+    const signInButton = screen.queryByRole("link", {
+      name: /sign in/i,
+    });
+    const signUpButton = screen.queryByRole("link", {
+      name: /sign up/i,
+    });
+
+    expect(signInButton).not.toBeInTheDocument();
+    expect(signUpButton).not.toBeInTheDocument();
+  });
+
+  test("sign out is visible", async () => {
+    await renderComponent();
+
+    const signOutButton = screen.getByRole("link", {
+      name: /sign out/i,
+    });
+
+    expect(signOutButton).toBeInTheDocument();
+    expect(signOutButton).toHaveAttribute("href", "/signout");
+  });
+});
 
 describe("when user is not signed in", () => {
   // createServer() ---> GET '/api/user' ---> { user: null }
@@ -49,22 +89,3 @@ describe("when user is not signed in", () => {
     expect(signOutButton).not.toBeInTheDocument();
   });
 });
-
-// describe('when user is signed in', () => {
-//   // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
-//   createServer([
-//     {
-//       path: '/api/user',
-//       res: () => {
-//         return { user: { id: 3, email: 'asdf@asdf.com' } };
-//       },
-//     },
-//   ]);
-
-//   test('sign in and sign up are not visible', async () => {
-//     renderComponent();
-//   });
-//   test('sign out is visible', async () => {
-//     renderComponent();
-//   });
-// });
